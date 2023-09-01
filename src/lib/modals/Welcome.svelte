63d9ca61896fea4f3ba9ec5d2995d8f0163da647
Dae314 <discord@dae314>
2023-08-31 17:18:10 -1000

    Add Welcome modal

---
+++ src/lib/modals/Welcome.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { getContext } from "svelte";
+	const { close } = getContext('simple-modal');
+
+	
+</script>
+
+<div>
+	<h2>Image goes here</h2>
+	<button type="button" on:click={() => close()}>Begin</button>
+</div>

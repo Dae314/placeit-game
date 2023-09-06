58a966927929b5fe322d77870c2a77bdb7163e5b
Dae314 <discord@dae314>
2023-09-06 09:19:41 -1000

    Updates

---
+++ _app/immutable/chunks/paths.8860bf9d.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1juse1k)==null?void 0:s.base)??"/placeit-game";var e;const t=((e=globalThis.__sveltekit_1juse1k)==null?void 0:e.assets)??a;export{t as a,a as b};

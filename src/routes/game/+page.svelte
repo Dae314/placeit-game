1cdac19a41f8247ca0ac562e5235008e88889488
Dae314 <discord@dae314>
2023-09-06 13:59:25 -1000

    Fix formatTime formatting when non-number is given

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -153,6 +153,7 @@
 	}
 
 	function formatTime(timeInMS) {
+		if(!Number.isInteger(timeInMS)) return '0:00';
 		let minutes = Math.floor(timeInMS / 60000);
 		let seconds = ((timeInMS % 60000) / 1000).toFixed(0);
 		return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;

1119d9b467f7ff2e1759ffd54516ac51a2c7d97b
Dae314 <discord@dae314>
2023-09-06 13:01:12 -1000

    Create initial about page and add version import from package.json

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { version } from '$app/environment';
+</script>
+
+<div class="container">
+	<p class="version">{version}</p>
+</div>
+
+<style lang="scss">
+	.version {
+		margin: 0;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+const path = fileURLToPath(new URL('package.json', import.meta.url));
+const pkg = JSON.parse(readFileSync(path, 'utf8'));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +16,9 @@ const config = {
 		adapter: adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/placeit-game' : '',
+		},
+		version: {
+			name: pkg.version
 		}
 	}
 };

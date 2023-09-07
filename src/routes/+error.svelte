bf49d0fc996888d646e265601c6ca290a6748103
Dae314 <discord@dae314>
2023-09-06 21:55:26 -1000

    Style error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,4 +2,16 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>{$page.status} {$page.error.message}</h1>
+<div class="container">
+	<h1>{$page.status} {$page.error.message}</h1>
+</div>
+
+<style lang="scss">
+	.container {
+		align-items: center;
+		display: flex;
+		height: 100%;
+		justify-content: center;
+		width: 100%;
+	}
+</style>

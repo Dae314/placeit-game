159bad2eead6fe796f20112c145852162bcddcd1
Dae314 <discord@dae314>
2023-09-07 22:22:07 -1000

    Implement rules video

--- src/lib/modals/Rules.svelte
+++ src/lib/modals/Rules.svelte
@@ -14,20 +14,10 @@
 
 <div class="container">
 	<h2>How to Play</h2>
-	<ol>
-		<li>
-			<img src="{base}/rules_images/roll-it.webp" alt="Roll It">
-		</li>
-		<li>
-			<img src="{base}/rules_images/click-it.webp" alt="Click It">
-		</li>
-		<li>
-			<img src="{base}/rules_images/place-it.webp" alt="Place It">
-		</li>
-		<li>
-			<img src="{base}/rules_images/roll-again.webp" alt="Roll Again">
-		</li>
-	</ol>
+	<video autoplay loop>
+		<source src="{base}/rules_resources/rules.webm" type="video/webm" />
+		<source src="{base}/rules_resources/rules.mp4" type="video/mp4" />
+	</video>
 	<button type="button" on:click={closeModal}>Close</button>
 </div>
 
@@ -37,9 +27,15 @@
 	}
 	h2 {
 		margin: 0;
+		margin-bottom: 20px;
 		text-align: center;
+	}
+	video {
+		height: auto;
+		width: 100%;
 	}
 	button {
 		margin: 0 auto;
+		margin-top: 20px;
 	}
 </style>
